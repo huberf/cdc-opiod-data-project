178749953a0c433178bed2e6617995b8fd8fe0c8
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-10-25 07:48:09 -0400

    Add updated analysis process

--- README.md
+++ README.md
@@ -5,7 +5,14 @@
   2015 grouped by factors such as year, weekday, gender, etc.
 * Opiod Deaths by County 1999-2015.txt - Includes 52,860 rows of data grouped
   by census region, county and year.
+* unemploymentByCounty.json - Includes unemployment data for every county from
+  1999 to 2016
 
 ## Helper Tools
 * boilerplateDataLoad.py - Automatically loads the main data set and can be
   copied and edited to do complex data analysis.
+
+## Analysis
+Under the `/analyis` subdirectory are python scripts to perform statistical
+operations on the data above. Currently the system supports correlating opioid
+deaths with unemployment data.
--- analysis/deathsAndUnemploymentRegression.py
+++ analysis/deathsAndUnemploymentRegression.py
@@ -4,50 +4,71 @@ import json
 
 # Load data into the x and y
 unemploymentFileName = '../unemploymentByCounty.json'
-unemploymentFile = json.load(file(unemploymentFileName))
+unemploymentFile = json.load(open(unemploymentFileName))
 
 deathFileName = '../DeathsByCountyUnsuppressed2010-2015.json'
-deathFile = json.load(file(deathFileName))
+deathFile = json.load(open(deathFileName))
 
-# x is arbitrarily death count and y is unemployment
-x = []
-y = []
+# TODO: Integrate data source which allows death data selection by year
+def getDeathCountByCounty(countyName, year):
+    return deathFile[countyName]
+
+# County data graph stores values for each county over the data lifetime
+countyDataGraph = {}
 noMatchCount = 0
 matchCount = 0
 # The death data file has county names mapped to values so we can do the below
 for i in unemploymentFile:
     try:
-        deaths = deathFile[i['county']]
-        x += [deaths]
-        y += [i['unemploymentRate']]
+        countyName = i['county']
+        deaths = getDeathCountByCounty(countyName, i['year'])
+        try:
+            countyDataGraph[countyName]
+        except:
+            countyDataGraph.update({countyName: []})
+        newData = [i['unemploymentRate'], deaths]
+        countyDataGraph[countyName] += [newData]
         matchCount += 1
     except:
         noMatchCount += 1
-
-
-print('--------- X ----------')
-print(x)
-print('--------- Y ----------')
-print(y)
-
-# Convert lists to Numpy arrays
-
-x_array = np.array(x).astype(np.float)
-y_array = np.array(y).astype(np.float)
-
-slope, intercept, r_value, p_value, std_err = stats.linregress(x_array, y_array)
 
 # Show meta process info
 print('Match count: ', matchCount)
 print('No match count: ', noMatchCount)
 print()
 
-# Show general stats
-print('Slope: ', slope)
-print('Intercept: ', intercept)
-print('r-value: ', r_value)
-print('p-value: ', p_value)
-print('Standard error: ', std_err)
+average_r_2_value = 0.0
+count = 0.0
+# Convert lists to Numpy arrays
+for key, value in countyDataGraph.iteritems():
+    x = []
+    y = []
+    for i in value:
+        x += [i[0]]
+        y += [i[1]]
+    print('--------- X ----------')
+    print(x)
+    print('--------- Y ----------')
+    print(y)
+    x_array = np.array(x).astype(np.float)
+    y_array = np.array(y).astype(np.float)
 
-# Show how well the model works
-print('r-squared: ', r_value**2)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_array, y_array)
+
+    currentRSum = average_r_2_value * count
+    count += 1.0
+    currentRSum += r_value
+    average_r_value = currentRSum/count
+
+    # Show general stats
+    print('Slope: ', slope)
+    print('Intercept: ', intercept)
+    print('r-value: ', r_value)
+    print('p-value: ', p_value)
+    print('Standard error: ', std_err)
+
+    # Show how well the model works
+    print('r-squared: ', r_value**2)
+
+print('--------------------------------')
+print('Average r-squared: ', average_r_2_value)
