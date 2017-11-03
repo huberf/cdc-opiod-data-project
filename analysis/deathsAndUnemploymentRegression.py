59ad4003ecfe9c5e5cd924e78bfc1b2cb4665d75
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-11-03 13:48:12 -0400

    Improve documentation in unemployment correlation file

--- analysis/deathsAndUnemploymentRegression.py
+++ analysis/deathsAndUnemploymentRegression.py
@@ -4,31 +4,34 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-# Load data into the x and y
+# Load unemployment data (local and national)
 unemploymentFileName = '../unemploymentByCounty.json'
 unemploymentFile = json.load(open(unemploymentFileName))
 nationalUnemployment = json.loads(open('../unemploymentNationalByYear.json').read())
 
 # deathFileName = '../DeathsByCountyUnsuppressed2010-2015.json'
 # deathFile = json.load(open(deathFileName))
+
+# Load opioid death data by year for all years included below
 deathData = {}
 years = ['1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
 for i in years:
     data = json.load(open('../opioid_deaths/' + i + '.json'))
     deathData.update({i: data})
 
-# TODO: Integrate data source which allows death data selection by year
+# Retreives the death per 1000000 by count name and year
 def getDeathCountByCounty(countyName, year):
     deathFile = deathData[year]
     deathSpecifics = deathFile[countyName]
     deathCount = deathSpecifics['Death']
     population = deathSpecifics['Population']
-    deathPercent = (deathCount * 100000.0)/population
- #   print(deathPercent)
-    return deathPercent
+    hundredThousand = 100000.0
+    deathPerPop = (deathCount) * (hundredThousand/population)
+    return deathPerPop
 
 # County data graph stores values for each county over the data lifetime
 countyDataGraph = {}
+# Shows how many counties could be mapped to data
 noMatchCount = 0
 matchCount = 0
 # The death data file has county names mapped to values so we can do the below
@@ -36,40 +39,49 @@ for i in unemploymentFile:
     try:
         countyName = i['county']
         deaths = getDeathCountByCounty(countyName, i['year'])
+        # Check if the data graph for the county name already been initialized
         try:
             countyDataGraph[countyName]
         except:
             countyDataGraph.update({countyName: []})
-        # Clean unemployment by using a method such as comparing to national average
+        # Clean unemployment by comparing to national average
         cleanedUnemployment = float(i['unemploymentRate']) - nationalUnemployment[i['year']]
         #cleanedUnemployment = i['unemploymentRate']
         newData = [cleanedUnemployment, deaths]
+        # Add the data to the county data
         countyDataGraph[countyName] += [newData]
         matchCount += 1
     except:
+        # As there isn't opioid data for every county this is called
         noMatchCount += 1
 
 # Show meta process info
-#print('Match count: ', matchCount)
-#print('No match count: ', noMatchCount)
-#print()
+print('Match count: ', matchCount)
+print('No match count: ', noMatchCount)
+print()
 
 average_r_2_value = 0.0
 count = 0.0
 maxRSquared = 0
 r_2_values = {}
+# Tracking of positive and negative correlation
 slopeCount = {'up': 0, 'down': 0}
+# Map of all slope values by county code
 slope_values = {}
 countyNameToCode = json.load(open('../CountyNameToCode.json'))
 # Convert lists to Numpy arrays
 index = 0
+# Long term collection of x and y values to be showed at the end
 x_acc = []
 y_acc = []
 
 for key, value in list(countyDataGraph.items()):
+    # Setup values to be viewed by the x and y
+    # X is unemployment and Y is death rate
     x = []
     y = []
     for i in value:
+        # Scrub for zero values
         if i[1] != 0 and i[0] != 0:
             x += [i[0]]
             y += [i[1]]
@@ -85,23 +97,28 @@ for key, value in list(countyDataGraph.items()):
     x_array = np.array(x).astype(np.float)
     y_array = np.array(y).astype(np.float)
 
+    # Generate linear regression values for data
     slope, intercept, r_value, p_value, std_err = stats.linregress(x_array, y_array)
 
+    # Show the county name
     print(key)
-    # Add the performance agains the national average compared to the slope
+    # Add the raw values normalized by division of the the slope
+    # limited to relations by states for which uenmployment is a factor
     # Supports filtering by the two character state abbreviation
     if (key[-2:] != "Random" and r_value**2 > 0.05):
         #x_acc += [np.average(x_array)]
         #y_acc += [slope]
-#
+        #
         #x_acc += x_array.tolist()
         #y_acc += np.divide(y_array, 1).tolist()
+        #
         # For graphing display values divided by the slope
         # to normalize
         x_acc += np.divide(x_array, slope).tolist()
         y_acc += np.divide(y_array, slope).tolist()
 
     index += 1
+    '''
     if index == -1:
         print(x_array)
         print(y_array)
@@ -109,23 +126,29 @@ for key, value in list(countyDataGraph.items()):
         plt.xlabel("Unemployment")
         plt.ylabel("Death Per 100000")
         plt.show()
+    '''
 
+    # Log whether the slope was positive or negative for this county
     if slope > 0:
         slopeCount['up'] += 1
     else:
         slopeCount['down'] += 1
 
+    # LEGACY: Detection of highest r^2 value
     if (r_value**2 > maxRSquared):
         print(key)
         print(r_value**2)
         maxRSquared = r_value**2
 
-    # Save useful data
+    # Save useful data such as r^2 and slope values
     countyCode = countyNameToCode[key]
     r_2_values.update({countyCode: r_value**2})
     slope_values.update({countyCode: slope})
 
-    if (r_value**2 > 0.05):
+    minimumCorrelation = 0.05
+    # Compute data set wide values limited by county
+    # classifications
+    if (r_value**2 > minimumCorrelation):
         currentRSum = average_r_2_value * count
         count += 1.0
         currentRSum += r_value**2
@@ -138,7 +161,7 @@ for key, value in list(countyDataGraph.items()):
     print('p-value: ', p_value)
     print('Standard error: ', std_err)
 
-    # Show how well the model works
+    # Show how well the model works for the specific county
     print('r-squared: ', r_value**2)
 
 # Write data to files
@@ -150,8 +173,10 @@ slopeCounties.write(json.dumps(slope_values))
 print('--------------------------------')
 print('Average r-squared: ', average_r_2_value)
 print('Using ' + str(count) + ' out of ' + str(index))
+print('Data set wide slope correlation: ')
 print(slopeCount)
 
+# Plot data set wide values tracked
 plt.plot(x_acc, y_acc, "ro", ms = "0.5")
 plt.xlabel("Unemployment vs. National Average / Slope")
 plt.ylabel("Death Per 100000 / Slope")
